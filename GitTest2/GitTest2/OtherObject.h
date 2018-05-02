8d84aacd392a0c759cb7b2092c73a89fbe9a4347
irawrmike <irmike@gmail.com>
2018-05-02 13:32:47 -0400

    added object

---
+++ GitTest2/GitTest2/OtherObject.h
@@ -0,0 +1,13 @@
+//
+//  OtherObject.h
+//  GitTest2
+//
+//  Created by Mike Stoltman on 2018-05-02.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OtherObject : NSObject
+
+@end
---
+++ GitTest2/GitTest2/OtherObject.m
@@ -0,0 +1,13 @@
+//
+//  OtherObject.m
+//  GitTest2
+//
+//  Created by Mike Stoltman on 2018-05-02.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import "OtherObject.h"
+
+@implementation OtherObject
+
+@end
