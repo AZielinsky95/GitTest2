4c04847a535292d6f99482bb208978315dd46832
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-05-02 13:05:58 -0400

    Color class

---
+++ GitTest2/GitTest2/Color.h
@@ -0,0 +1,13 @@
+//
+//  Color.h
+//  GitTest2
+//
+//  Created by Alejandro Zielinsky on 2018-05-02.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Color : NSObject
+
+@end
---
+++ GitTest2/GitTest2/Color.m
@@ -0,0 +1,13 @@
+//
+//  Color.m
+//  GitTest2
+//
+//  Created by Alejandro Zielinsky on 2018-05-02.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import "Color.h"
+
+@implementation Color
+
+@end
